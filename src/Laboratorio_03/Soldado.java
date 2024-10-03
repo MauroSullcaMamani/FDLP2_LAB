f2c70e81e61956607e561a380a79154c9091927e
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-10-02 23:14:51 -0500

    1ra version

---
+++ src/Laboratorio_03/Soldado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FP2_Lab03;
+
+/**
+ *
+ * @author Mauro Snayder
+ */
+public class Soldado {
+    private String nombre;
+    private int vida;
+}
