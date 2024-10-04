0972df5106bfb3c74c31ee6f91d328fc588bef37
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-10-04 16:17:39 -0500

    terminado

--- src/Laboratorio_03/Soldado.java
+++ src/Laboratorio_03/Soldado.java
@@ -11,4 +11,23 @@ package FP2_Lab03;
 public class Soldado {
     private String nombre;
     private int vida;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public String toString() {
+        return nombre+"\t->"+vida;
+    }
 }
