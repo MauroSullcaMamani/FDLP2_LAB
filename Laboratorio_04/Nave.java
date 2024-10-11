3f738aa91bbf0b6461b78fb5c9867b69a99c0cc2
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-10-11 18:21:50 -0500

    avance 1

--- Laboratorio_04/Nave.java
+++ Laboratorio_04/Nave.java
@@ -9,5 +9,60 @@ package laboratorio_5.DemoBatalla;
  * @author Usuario24B
  */
 public class Nave {
+     // Creamos los atributos
+    private String nombre;
+    private int fila;
+    private String columna;
+    private boolean estado;
+    private int puntos;
     
+    // Creamos los Set y Get de cada atributo
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public String getColumna() {
+        return columna;
+    }
+
+    public void setColumna(String columna) {
+        this.columna = columna;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+    // Creamos el toString y un metodo que imprimirá el nombre y puntos
+    public String toString() {
+        return "Nave{" + "nombre=" + nombre + ", fila=" + fila + ", columna=" + columna + ", estado=" + estado + ", puntos=" + puntos + '}';
+    }
+    
+    public String navePunto() {
+        return "Nave{" + "nombre=" + nombre + ", puntos=" + puntos + '}';
+    }
 }
