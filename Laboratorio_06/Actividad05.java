bf74aca08f734c98678ec190c5f2ea5d5cc40046
MauroSullcaMamani <msullcam@unsa.edu.pe>
2024-10-23 23:04:40 -0500

    terminado

--- Laboratorio_06/Actividad05.java
+++ Laboratorio_06/Actividad05.java
@@ -2,23 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package FP2_Lab06;
+package Laboratorio_06;
 
 /**
  *
- * @author Usuario24B
+ * @author Mauro Snayder
  */
 import java.util.*;
 
 public class Actividad05 {
     public static void main(String[] args) {
-        // Inicializamos dos ejércitos con un número aleatorio de soldados entre 1 y 5
         int fila=10;//Generamos la fila de la tabla
         int columna=10;//Generamos la columna de la tabla
-        ArrayList<ArrayList<Soldado>> tabla=new ArrayList<ArrayList<Soldado>>();
-        ArrayList<Soldado> ejercito1=new ArrayList<Soldado>();
-        ArrayList<Soldado> ejercito2=new ArrayList<Soldado>();
-        for (int i=0;i<fila;i++){
+        ArrayList<ArrayList<Soldado>> tabla=new ArrayList<ArrayList<Soldado>>();//Creamos la tabla
+        ArrayList<Soldado> ejercito1=new ArrayList<Soldado>();//Creamos ejercito 1
+        ArrayList<Soldado> ejercito2=new ArrayList<Soldado>();//Creamos ejercito 2
+        for (int i=0;i<fila;i++){//Inicializamos el tablero con valores vacios
             tabla.add(new ArrayList<Soldado>());
             for (int j=0;j<columna;j++){
                 tabla.get(i).add(new Soldado());
@@ -41,18 +40,18 @@ public class Actividad05 {
     }
     // Método para inicializar una tabla con cierto numeros de soldados
     public static void inicializarEjercito(ArrayList<ArrayList<Soldado>> tabla,ArrayList<Soldado> ejercito,int tipo){
-        int numSoldados=(int) (Math.random()*10+1);
+        int numSoldados=(int) (Math.random()*10+1);//Genemos la cantidad de soldados
         for (int i=0;i<numSoldados;i++){
-            Soldado persona=new Soldado();//creamos "persona" para luego ponerlo dentro del arreglo
+            Soldado persona=new Soldado();//creamos "persona" para luego ponerlo dentro del tablero y del Array del ejercito
             do {
                 persona.aleatorioPosicion(tabla.size(),tabla.get(0).size());//generamos una posicion
                 persona.aleatorioVida();//generamos la vida
-                persona.setNombre("soldado"+persona.getFila()+"X"+persona.getColumna()+" E"+tipo);
+                persona.setNombre("soldado"+persona.getFila()+"X"+persona.getColumna()+" E"+tipo);//Generamos el nombre del soldado
             }
             //El bucle se repite si en una posicion aleatoria ya existe un soldado puesto
             while(!tabla.get(persona.getFila()).get(persona.getColumna()).getNombre().equals("             "));
-            ejercito.add(persona);//ponemos el soldado dentro del arreglo
-            tabla.get(persona.getFila()).set(persona.getColumna(),persona);
+            ejercito.add(persona);//ponemos el soldado dentro del Array del ejercito
+            tabla.get(persona.getFila()).set(persona.getColumna(),persona);//ponemos el soldado dentro del tablero
         }
     }
     // Metodo para mostrar la tabla
@@ -69,8 +68,8 @@ public class Actividad05 {
     //Metodo para determinar el soldado con mayor vida
     public static Soldado mayorVida(ArrayList<Soldado> ejercito){
         Soldado mayor=new Soldado();//creamos un objeto para almacenar al soldado mayor
-        for (Soldado persona:ejercito){
-            if (persona.getVida()>mayor.getVida())
+        for (Soldado persona:ejercito){ //Recorremos todo el Array del ejercito
+            if (persona.getVida()>mayor.getVida())//Buscamos al soldado con mayor vida
                 mayor=persona;          
         }
         return mayor;
@@ -78,31 +77,31 @@ public class Actividad05 {
     //Metodo para determinar la vida total de todos los soldados
     public static double vidaPromedio(ArrayList<Soldado> ejercito){
         double vidaT=0;//vida inicial
-        for (Soldado persona:ejercito){
-            vidaT+=persona.getVida();
+        for (Soldado persona:ejercito){//Recorremos todo el Array del ejercito
+            vidaT+=persona.getVida();//Sumamos las vidas de todos los soldados
         }
         return vidaT/ejercito.size();
     }
     //Metodo para ver las lista de los soldados por el orden de creacion
     public static void mostrarEjercitoOrdenCreacion(ArrayList<Soldado> ejercito,int tipo){
         System.out.println("Ejercito "+tipo+" : ");
-        for (Soldado persona:ejercito){//imprimimos el arreglo
+        for (Soldado persona:ejercito){//imprimimos todos los soldados del Array ejercito
             System.out.print(persona.toString());
         }
     }
-    //Metodo para ver el ranking de los soldados (por vida)
+    //Metodo para ver el ranking de los soldados version 1(por vida)
     public static void rankingSoldadosV1(ArrayList<Soldado> ejercito,int tipo){
-        ordenamientoBurbuja(ejercito);//ordenamos el arreglo unidimensional
+        ordenamientoBurbuja(ejercito);//ordenamos el Array Unidimencional
         System.out.println("Ejercito "+tipo+" : ");
-        for (int i=ejercito.size()-1;i>=0;i--){//imprimimos el arreglo
+        for (int i=ejercito.size()-1;i>=0;i--){//imprimimos el Array
             System.out.print(ejercito.get(i).toString());
         }
     }
-    //Metodo para ver el ranking de los soldados (por vida)
+    //Metodo para ver el ranking de los soldados version 2(por vida)
     public static void rankingSoldadosV2(ArrayList<Soldado> ejercito,int tipo){
-        ordenamientoInsercion(ejercito);//ordenamos el arreglo unidimensional
+        ordenamientoInsercion(ejercito);//ordenamos el Array Unidimencional
         System.out.println("Ejercito "+tipo+" : ");
-        for (int i=ejercito.size()-1;i>=0;i--){//imprimimos el arreglo
+        for (int i=ejercito.size()-1;i>=0;i--){//imprimimos el Array
             System.out.print(ejercito.get(i).toString());
         }
     }
